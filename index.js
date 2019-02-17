e8761bc5dd1245c53135f038a43dc606c5aa2d93
Fabian Fabro <fabrofabian@gmail.com>
2019-02-17 06:32:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ function appendKitten(newKitten){
 }
 
 function prependKitten(newKitten){
-  var newArray = kittens.concat(newKitten);
+  var newArray = newKitten.concat(kittens);
   return newArray;
 }
